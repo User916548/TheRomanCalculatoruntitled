2e940670f66f6abb824399378148c83b8a66cd98
user916548 <i.u.coder@yandex.ru>
2024-01-18 20:29:06 +0300

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 12, b = 58;
+        System.out.println(a + b);
+
     }
 }
